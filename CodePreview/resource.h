497a65ded0898a94f9107d5e79768a5098e3fcea
Dennis Felsch <dennis.felsch@ruhr-uni-bochum.de>
2011-11-27 16:56:41 +0100

    Release version 0.9.2

---
+++ CodePreview/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CodePreview.rc
+//
+#define IDD_MAINDIALOG                  101
+#define IDC_STATIC_TITLE                1001
+#define IDC_STATIC_PICTURE              1002
+#define IDC_RICHEDIT21                  1003
+#define IDC_EDIT_COMMENTS               1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CodePreview.rc
+
+// Nächste Standardwerte für neue Objekte
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
